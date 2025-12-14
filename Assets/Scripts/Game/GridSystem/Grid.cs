38bdaec075e77127d299d5f4bc9e97db33ef1c9a
waitwhater <waitwhater@yandex.ru>
2025-12-15 00:09:07 +0300

    created grid

---
+++ Assets/Scripts/Game/GridSystem/Grid.cs
@@ -0,0 +1,45 @@
+﻿using Assets.Scripts.Game.Tiles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.Game.GridSystem
+{
+    class Grid
+    {
+        public Tile[,] GameGrid { get; private set; }
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+
+        public Vector2Int CurrentPosition { get; private set; }
+        public Vector2Int TargetPosition { get; private set; }
+
+        public Grid(int height, int width)
+        {
+            Height = height;
+            Width = width;
+            GameGrid = new Tile[Height, Width];
+        }
+
+        public Vector2Int SetCurrentPosition(Vector2Int position) => CurrentPosition = position;
+        public Vector2Int SetTargetPosition(Vector2Int position) => TargetPosition = position;
+
+        public Vector3 GridToWorld(int x, int y) => new Vector3(x, y, 0);
+
+        public Vector2Int WorldToGrid(Vector3 worldPosition)
+        {
+            var x = Mathf.FloorToInt(worldPosition.x);
+            var y = Mathf.FloorToInt(worldPosition.y);
+            return new Vector2Int(x, y);
+        }
+
+        public bool IsValidPosition (int x, int y)
+        {
+            return x >=0 
+                && y >=0 
+                && x < Height 
+                && y < Width;
+        }
+    }
+}

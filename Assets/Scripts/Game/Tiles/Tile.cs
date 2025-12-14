2d12af8d09b9e9d22540dcb4369dea3ec2192be9
waitwhater <waitwhater@yandex.ru>
2025-12-14 21:56:38 +0300

    add tile scripts and set1 sprites
    add tile scripts and set1 sprites

---
+++ Assets/Scripts/Game/Tiles/Tile.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Assets.Scripts.Game.Tiles
+{
+	[RequireComponent(typeof(SpriteRenderer))]
+	public class Tile : MonoBehaviour
+	{
+		public TileConfig TileConfig { get; private set; }
+		public bool IsMatched;
+		public bool IsInteractable;
+
+		public void SetTileConfig (TileConfig tileConfig)
+		{
+			TileConfig = tileConfig;
+			IsMatched = false;
+			IsInteractable = tileConfig.IsInteractable;
+			GetComponent<SpriteRenderer>().sprite = tileConfig.Sprite;
+		}
+
+        public bool SetMatch(bool value) => IsMatched = value;
+    }
+}
---
+++ Assets/Scripts/Game/Tiles/TileConfig.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Assets.Scripts.Game.Tiles
+{
+	public enum TileKind 
+	{
+		Normal, 
+		Blank, 
+		Jelly
+	}
+
+	[CreateAssetMenu(fileName = "TileConfig", menuName = "Config/TileConfig")]
+	public class TileConfig : ScriptableObject
+	{
+        [SerializeField] private Sprite _sprite;
+		[SerializeField] private TileKind _tileKind;
+		[SerializeField] private bool _isInteractable;
+
+        public Sprite Sprite { get => _sprite; }
+        public TileKind TileKind { get => _tileKind; }
+        public bool IsInteractable { get => _isInteractable; }
+    }
+}

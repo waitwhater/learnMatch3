6f02810b03b6b82d847cf4f1455ba1007b6d5215
waitwhater <waitwhater@yandex.ru>
2025-12-16 01:24:32 +0300

    part 1 of DI lesson

---
+++ Assets/Scripts/DI/GameScope.cs
@@ -0,0 +1,24 @@
+﻿using Assets.Scripts.Game.Board;
+using Assets.Scripts.Game.GridSystem;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using VContainer;
+using VContainer.Unity;
+
+namespace Assets.Scripts.DI
+{
+    internal class GameScope : LifetimeScope
+    {
+        [SerializeField] private GameBoard _gameBoard;
+
+        protected override void Configure(IContainerBuilder builder)
+        {
+            builder.Register<Game.GridSystem.Grid>(Lifetime.Singleton);
+            builder.RegisterInstance(_gameBoard);
+
+            //base.Configure(builder);
+        }
+    }
+}
---
+++ Assets/Scripts/Game/Board/GameBoard.cs
@@ -0,0 +1,52 @@
+﻿using Assets.Scripts.Game.Tiles;
+using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VContainer;
+
+namespace Assets.Scripts.Game.Board
+{
+    public class GameBoard : MonoBehaviour
+    {
+        [SerializeField] private GameObject _tilePrefab;
+        private readonly List<Tile> _tilesToRefill = new (); 
+        private GridSystem.Grid _grid;
+
+        public void CreateBoard ()
+        {
+
+        }
+
+        private void FillBoard() 
+        {
+            for (int x = 0; x < _grid.Width; x++) 
+                for (int y = 0; y < _grid.Height; y++)
+                {
+                    if (_grid.GetValue(x, y) != null)
+                        continue;
+                    var tile = Instantiate(_tilePrefab, transform);
+                    tile.transform.position = _grid.GridToWorld(x, y);
+                    _grid.SetValue(x, y, tile.GetComponent<Tile>());
+                }
+        } 
+
+        [Inject] private void Construct (GridSystem.Grid grid)
+        {
+            _grid = grid;
+        }
+        /*
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        */
+    }
+}

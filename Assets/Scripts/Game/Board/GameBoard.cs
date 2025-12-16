f311980a1f6dc364bec0fdf93e536d7aceaaa473
waitwhater <waitwhater@yandex.ru>
2025-12-17 00:09:41 +0300

    2nd lesoon of DI inj

--- Assets/Scripts/Game/Board/GameBoard.cs
+++ Assets/Scripts/Game/Board/GameBoard.cs
@@ -10,16 +10,24 @@ namespace Assets.Scripts.Game.Board
     public class GameBoard : MonoBehaviour
     {
         [SerializeField] private GameObject _tilePrefab;
+        [SerializeField] private TileConfig _tileConfig;
         private readonly List<Tile> _tilesToRefill = new (); 
         private GridSystem.Grid _grid;
 
+        void Start()
+        {
+            CreateBoard();
+        }
+
         public void CreateBoard ()
         {
-
+            FillBoard();
         }
 
         private void FillBoard() 
         {
+            _grid = new GridSystem.Grid(10, 10); //временное решение для создания сетки
+
             for (int x = 0; x < _grid.Width; x++) 
                 for (int y = 0; y < _grid.Height; y++)
                 {
@@ -27,7 +35,9 @@ namespace Assets.Scripts.Game.Board
                         continue;
                     var tile = Instantiate(_tilePrefab, transform);
                     tile.transform.position = _grid.GridToWorld(x, y);
-                    _grid.SetValue(x, y, tile.GetComponent<Tile>());
+                    var tileComponent = tile.GetComponent<Tile>();
+                    tileComponent.SetTileConfig(_tileConfig);
+                    _grid.SetValue(x, y, tileComponent);
                 }
         } 
 

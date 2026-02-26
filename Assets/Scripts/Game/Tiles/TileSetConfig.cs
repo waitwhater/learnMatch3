650fb8d2885720e86f49442b4c151ada5964a671
waitwhater <waitwhater@yandex.ru>
2026-02-26 23:58:18 +0300

    Create TileSetConfig.cs
    старт урока по pool objects

---
+++ Assets/Scripts/Game/Tiles/TileSetConfig.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Assets.Scripts.Game.Tiles
+{
+	public class TileSetConfig: ScriptableObject
+	{
+
+
+		[CreateAssetMenu(fileName = "TileSetConfig", menuName = "Configs/TileSetConfig")]
+
+
+		//static void doit()
+		//{
+		//	editorutility.displaydialog("mytool", "do it in c# !", "ok", "");
+		//}
+	}
+}

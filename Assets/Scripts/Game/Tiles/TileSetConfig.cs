79e1c0e270719abb12c82ab8bfc846906bffb00c
waitwhater <waitwhater@yandex.ru>
2026-02-27 00:04:21 +0300

    Update TileSetConfig.cs

--- Assets/Scripts/Game/Tiles/TileSetConfig.cs
+++ Assets/Scripts/Game/Tiles/TileSetConfig.cs
@@ -3,16 +3,10 @@ using UnityEditor;
 
 namespace Assets.Scripts.Game.Tiles
 {
-	public class TileSetConfig: ScriptableObject
+    [CreateAssetMenu(fileName = "TileSetConfig", menuName = "Configs/TileSetConfig")]
+    public class TileSetConfig: ScriptableObject
 	{
 
 
-		[CreateAssetMenu(fileName = "TileSetConfig", menuName = "Configs/TileSetConfig")]
-
-
-		//static void doit()
-		//{
-		//	editorutility.displaydialog("mytool", "do it in c# !", "ok", "");
-		//}
 	}
 }
